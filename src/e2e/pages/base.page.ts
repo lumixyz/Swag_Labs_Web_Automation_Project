d4beb90c22bba5c85d38c61bdcfbb9c636b65680
lumixyz <pelumiolajide20@gmail>
2024-06-20 18:15:25 +0100

    products page objects 2

--- src/e2e/pages/base.page.ts
+++ src/e2e/pages/base.page.ts
@@ -9,7 +9,14 @@ export default class Base{
     }
 
     //Common Actions
-    getEle(element: string){
-        return this.page.locator(element);
+    getEle(locator: string){
+        return this.page.locator(locator);
+      }
+
+      async getMultiEles(multiLocator: string, eleText: string){
+        const elements = await this.page.$$(multiLocator);
+        for(const ele of elements){
+            if (await ele.textContent() == eleText) ele.click();
+        }
       }
 }
--- src/e2e/pages/product.page.ts
+++ src/e2e/pages/product.page.ts
@@ -8,16 +8,54 @@ export default class Product extends Base{
 
    //Locators
    menu = '#react-burger-menu-btn';
+   menuList = ".bm-item-list>a";
+   closeMenuBtn = '#react-burger-cross-btn';
    pageTitle = '.title';
    filter = '.product_sort_container';
    cart = '.shopping_cart_link';
-   itemsList = "//div[@class='inventory_list']//a[contains(@data-test, 'title')]";
+   itemsList = "//div[@class='inventory_list']//a[contains(@data-test, 'title')]//div";
    addToCartBtnList = "//div[@class='inventory_list']//button";
    priceList = "//div[@class='inventory_item_price']";
 
 
    //Actions
-    async setUname(username: string){
-        await this.page.locator(this.menu).fill(username);
+    async getMenu(){
+        await this.page.locator(this.menu).click();
     }
+
+    async getAllItems(){
+        await this.page.locator(this.menuList).first().click();
+    }
+
+    async getAboutPage(){
+        await this.page.locator(this.menuList).nth(2).click();
+    }
+
+    async logout(){
+        await this.page.locator(this.menuList).nth(3).click();
+    }
+
+    async resetApp(){
+        await this.page.locator(this.menuList).last().click();
+    }
+
+    async closeMenu(){
+        await this.page.locator(this.closeMenuBtn).click();
+    }
+
+    async getCart(){
+        await this.page.locator(this.cart).click();
+    }
+
+    //values: az, za, hilo, lohi
+    async filterBy(value: string){
+        await this.page.locator(this.filter).selectOption({value: value});
+    }
+
+    async getItem(name: string){
+        this.getMultiEles(this.itemsList, name);
+    }
+
+    
+    
 }
